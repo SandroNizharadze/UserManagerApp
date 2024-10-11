50c961c6ca457be1a388cf35a880e21b705b748c
Sandro <admin@Sandora.local>
2024-10-11 22:02:07 +0400

    initial commit

---
+++ Main.cs
@@ -0,0 +1,11 @@
+namespace Practice;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        UserManager manager = new UserManager();
+        manager.Start();
+    }
+    
+}
